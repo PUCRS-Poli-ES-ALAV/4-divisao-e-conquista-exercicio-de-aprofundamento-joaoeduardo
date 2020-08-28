18b251431b978879bb712a0f3c4e67f045c3fe40
Eduardo André Soares <eduardo.andre.soares@icloud.com>
2020-08-28 13:05:59 -0300

    Questão de 1 a 3 prontas

---
+++ Algoritmos.java
@@ -0,0 +1,105 @@
+import java.util.Random;
+
+public class Algoritmos {
+
+    private int iteracao1;
+    private int iteracao2;
+    private int iteracao3;
+
+    public int getIteracao3() {
+        return iteracao3;
+    }
+
+    public int getIteracao2() {
+        return iteracao2;
+    }
+
+    public int getIteracao1() {
+        return iteracao1;
+    }
+
+    public int[] gerarVetor(int tamanho) {
+        Random gerador = new Random();
+        int[] vetAux = new int[tamanho];
+
+        for (int i = 0; i < tamanho; i++) {
+            vetAux[i] = gerador.nextInt(tamanho + 1000);
+            if (vetAux[i] < 0) {
+                vetAux[i] = vetAux[i] * -1;
+            }
+        }
+        return vetAux;
+    }
+
+    // NÚMERO 1
+
+    public void doMerge(int[] numbers, int left, int mid, int right, int tamanho) {
+        int[] temp = new int[tamanho];
+        int i, left_end, num_elements, tmp_pos;
+
+        left_end = (mid - 1);
+        tmp_pos = left;
+        num_elements = (right - left + 1);
+
+        while ((left <= left_end) && (mid <= right)) {
+            if (numbers[left] <= numbers[mid])
+                temp[tmp_pos++] = numbers[left++];
+            else
+                temp[tmp_pos++] = numbers[mid++];
+        }
+
+        while (left <= left_end)
+            temp[tmp_pos++] = numbers[left++];
+
+        while (mid <= right)
+            temp[tmp_pos++] = numbers[mid++];
+
+        for (i = 0; i < num_elements; i++) {
+            numbers[right] = temp[right];
+            right--;
+        }
+    }
+
+    public void mergeSort_Recursive(int[] numbers, int left, int right, int tamanho) {
+        iteracao1++;
+        int mid;
+        if (right > left) {
+            mid = (right + left) / 2;
+            mergeSort_Recursive(numbers, left, mid, tamanho);
+
+            mergeSort_Recursive(numbers, (mid + 1), right, tamanho);
+
+            doMerge(numbers, left, (mid + 1), right, tamanho);
+        }
+    }
+
+    // NÚMERO 2
+    public int maxVal1(int A[], int n) {
+        int max = A[0];
+        for (int i = 1; i < n; i++) {
+            if (A[i] > max)
+                max = A[i];
+            iteracao2++;
+        }
+        return max;
+    }
+
+    // NÚMERO 3
+    public int maxVal2(int A[], int init, int end) {
+        iteracao3++;
+        if (end - init <= 1) {
+            return max(A[init], A[end]);
+        } else {
+            int m = (init + end) / 2;
+            int v1 = maxVal2(A, init, m);
+            int v2 = maxVal2(A, m + 1, end);
+            return max(v1, v2);
+        }
+    }
+
+    public int max(int init, int end) {
+        if (end > init)
+            return end;
+        return init;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,59 @@
+
+public class Main {
+
+    static long startTime;
+    static long endTime;
+    static double v;
+    static Algoritmos algoritmo = new Algoritmos();
+
+    public static void main(String[] args) {
+
+        int tamanho = 1048576;
+        int[] vetor = algoritmo.gerarVetor(tamanho);
+
+        // // Teste do vetor aleatório
+        // for (int i = 0; i < vetor.length; i++) {
+        //     System.out.println(i + " - " + vetor[i]);
+        // }
+
+        exibirQuestao1(vetor, tamanho);
+        exibirQuestao2(vetor, tamanho);
+        exibirQuestao3(vetor, tamanho);
+
+
+    }
+
+    public static void exibirQuestao1(int[] vetor, int tamanho) {
+
+        startTime = System.currentTimeMillis();
+        algoritmo.mergeSort_Recursive(vetor, 0, vetor.length - 1, tamanho);
+        endTime = System.currentTimeMillis();
+        v = (double) (endTime - startTime) / 100;
+        System.out.println("Tempo gasto questão 1: " + v); // Mostra o tempo
+
+        // Vetor ordenado
+        for (int i = 0; i < tamanho; i++)
+            System.out.println(vetor[i]);
+
+        System.out.println("Numero de iteracoes: " + algoritmo.getIteracao1());
+
+    }
+
+    public static void exibirQuestao2(int[] vetor, int tamanho) {
+        startTime = System.currentTimeMillis();
+        System.out.println("Resultado da questão 2: " + algoritmo.maxVal1(vetor, tamanho));
+        endTime = System.currentTimeMillis();
+        v = (double) (endTime - startTime) / 100;
+        System.out.println("Número de iterações: " + algoritmo.getIteracao2());
+        System.out.println("Tempo gasto questão 2: " + v);
+    }
+
+    public static void exibirQuestao3(int[] vetor, int tamanho) {
+        startTime = System.currentTimeMillis();
+        System.out.println("Resultado da questão 3: " + algoritmo.maxVal2(vetor, 0, vetor.length - 1));
+        System.out.println("Número de iteração: " + algoritmo.getIteracao3());
+        endTime = System.currentTimeMillis();
+        v = (double) (endTime - startTime) / 100;
+        System.out.println("Tempo gasto questão 3: " + v);
+    }
+}
